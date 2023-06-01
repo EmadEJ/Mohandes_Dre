459f6873e852e8b63f4db71e1fc73f7ece7325ce
EmadEJ <s.emad.emamjomeh@gmail.com>
2023-06-01 10:17:52 +0330

    hmmmmm2

--- predictor.v
+++ predictor.v
@@ -8,18 +8,18 @@ module predictor(input wire request, result, clk, taken, output reg prediction);
         end
         if(result == 1) begin
             if(taken) begin
-                if(BHT[BRT] != 2'b11) begin
-                    BHT[BRT] += 1;
+                if(BHT[BRH] != 2'b11) begin
+                    BHT[BRH] += 1;
                 end
             end 
             else begin
-                if(BHT[BRT] != 2'b00) begin
-                    BHT[BRT] -= 1;
+                if(BHT[BRH] != 2'b00) begin
+                    BHT[BRH] -= 1;
                 end 
             end
             BRH = BRH << 1;
             BRH[0] = taken;
         end
     end
-    
+
 endmodule
